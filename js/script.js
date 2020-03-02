cadb21fedd26b1d84c21087788929f0be5c6f123
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-03-02 18:19:12 +0300

    add new fun

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 document.getElementById('yes').onclick = function() {
-    
+    var pass1 = document.getElementById('pass1').value;
+    var pass2 = document.getElementById('pass2').value;
+
+    if (pass1 == pass2) {
+        document.getElementById('out').innerHTML = 'Подтверждено';
+    } else if (pass1 == '' || pass2 == '' || pass1 == '' && pass2 == '') {
+        document.getElementById('out').innerHTML = 'Вы не ввели пароль! Введите пароль';
+    } else {
+        document.getElementById('out').innerHTML = 'Неверно! Попробуйте ещё раз';
+    };
 };
